f837b28291f042a38cccb2f00289d1e48795c158
Dezeabasili <ezeabasili@yahoo.co.uk>
2024-04-02 08:36:04 -0500

    Ready to deploy

--- utils/corsOrigins.js
+++ utils/corsOrigins.js
@@ -1,5 +1,6 @@
 const corsOrigins = [
-    "https://mysql-meridian-hosts-frontend.onrender.com"
+    // "https://mysql-meridian-hosts-frontend.onrender.com",
+    "https://aiven-meridian-hosts-frontend.onrender.com"
 ]
 
 module.exports = corsOrigins
--- utils/mysqlConnectionWithPromise.js
+++ utils/mysqlConnectionWithPromise.js
@@ -4,21 +4,21 @@ const fs = require("fs");
 const path = require("path")
 
 const db =  async () => {
-  // const conn = await mysql.createConnection({
-  //   host: process.env.AIVEN_HOST,
-  //   user: process.env.AIVEN_USER,
-  //   password: process.env.AIVEN_PASSWORD,
-  //   database: process.env.AIVEN_DATABASE,
-  //   port: process.env.AIVEN_PORT,
-  //   ssl: {
-  //     rejectUnauthorized: true,
-  //     // ca: fs.readFileSync("./ca.pem").toString(),
-  //     ca: fs.readFileSync("./ca.pem").toString(),
-  //   },
+  const conn = await mysql.createConnection({
+    host: process.env.AIVEN_HOST,
+    user: process.env.AIVEN_USER,
+    password: process.env.AIVEN_PASSWORD,
+    database: process.env.AIVEN_DATABASE,
+    port: process.env.AIVEN_PORT,
+    ssl: {
+      rejectUnauthorized: true,
+      // ca: fs.readFileSync("./ca.pem").toString(),
+      ca: fs.readFileSync("./ca.pem").toString(),
+    },
 
-  // })
+  })
 
-  const conn = await mysql.createConnection(process.env.DATABASE_URL)
+  // const conn = await mysql.createConnection(process.env.DATABASE_URL)
 
       return conn
 }
